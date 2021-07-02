013924587d4a6e4ff498647e89ad86635cc7b6e6
John <53703691+ShepstarShepherd@users.noreply.github.com>
2021-07-02 11:45:43 +0300

    Done week 4

---
+++ BasicFilmDbProject/Models/FilmForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace BasicFilmDbProject.Models
+{
+    public class FilmForm
+
+    {
+        [Key]
+       
+        public int FilmID { get; set; }
+        [Required(ErrorMessage = "All good films need a name")]
+        public string FilmTitle { get; set; }
+        [Required(ErrorMessage = "All good films need a age rating")]
+        public string FilmCertificate { get; set; }
+
+        public string FilmDescription { get; set; }
+
+        public string FilmImage { get; set; }
+        public decimal FilmPrice { get; set; }
+
+        public int Stars { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        
+
+    }
+
+}
